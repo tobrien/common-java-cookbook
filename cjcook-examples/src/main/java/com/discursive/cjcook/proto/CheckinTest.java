557ae5360b742d434696db2ff5e7c5e30499a44f
Tim O'Brien <tobrien@discursive.com>
2010-08-04 07:14:45 -0500

    Updated the Protocol Buffers chapter

--- cjcook-examples/src/main/java/com/discursive/cjcook/proto/CheckinTest.java
+++ cjcook-examples/src/main/java/com/discursive/cjcook/proto/CheckinTest.java
@@ -5,26 +5,24 @@ import java.util.Date;
 import org.apache.webdav.lib.properties.GetContentLengthProperty;
 
 import com.discursive.cjcook.proto.Checkin.PositionData;
+import com.google.protobuf.InvalidProtocolBufferException;
 
 public class CheckinTest {
 
-	
-	public static void main(String[] args) {
-		
-		PositionData pData = PositionData.newBuilder()
-			.setVehicleId(1)
-			.setTimestamp( (new Date()).getTime() )
-			.setAltitude( 0.0f )
-			.setLatitude( 0.0f )
-			.setLongitude( 0.0f )
-			.setSpeed( 120 )
-			.build();
-		
-		long time1 = (new Date()).getTime();
-		byte[] positionBArray = pData.toByteArray();
-		long time2 = (new Date()).getTime();
-		
-		
+	public static void main(String[] args) throws Exception {
+
+		// Create a new PositionData Message
+		PositionData pData = PositionData.newBuilder().setVehicleId(1)
+				.setTimestamp((new Date()).getTime()).setAltitude(0.0f)
+				.setLatitude(0.0f).setLongitude(0.0f).setSpeed(120).build();
+
+		// Convert the PositionData Message to a Byte Array
+		byte[] array = pData.toByteArray();
+		System.out.printf("Position Data message %d bytes", array.length);
+
+		// Parse the Byte Array into a PositionData message
+		PositionData parsed = PositionData.parseFrom(array);
+
 	}
-	
+
 }
